5cba74946dda7d75e26285ec19c9199feaf3cc69
Teresa Keller <56941889+tkeller333@users.noreply.github.com>
2021-09-15 19:57:54 -0500

    render new user form

--- SpaDay/Models/User.cs
+++ SpaDay/Models/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,7 +9,9 @@ namespace SpaDay.Models
     public class User
     {
         private string Username { get; set; }
+        [EmailAddress]
         private string Email { get; set; }
+        
         private string Password { get; set; }
 
         public User() { }
